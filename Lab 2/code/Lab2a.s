93844a8558c61d77f369f89e7502dbcdc40a6e26
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-03-05 15:24:43 -0700

    DONE

--- Lab 2/code/Lab2a.s
+++ Lab 2/code/Lab2a.s
@@ -14,7 +14,7 @@ movem.l %d2-%d7/%a2-%a5,(%a7)
 /* Names of Students: Arun Woosaree and Navras Kamal             **/
 /* Date: February 12, 2018                                       **/
 /* General Description:                                          **/
-/*                                                               **/
+/* Exploring different methods of indirect memory address modes  **/
 /******************************************************************/
 
 /*Write your program here******************************************/
@@ -23,11 +23,11 @@ movem.l %d2-%d7/%a2-%a5,(%a7)
 1.  0x2300000 - Size of array
 2.  0x2300004 - address of first array
 3.  0x2300008 - address of second array
-4.  0x230000C - address of where to store the sum with 
+4.  0x230000C - address of where to store the sum with
                 Register Indirect With Offset
-5.  0x2300010 - address of where to store the sum with 
+5.  0x2300010 - address of where to store the sum with
                 Indexed Register Indirect
-6.  0x2300014 - address of where to store the sum with 
+6.  0x2300014 - address of where to store the sum with
                 Postincrement Register Indirect
 */
 
@@ -41,9 +41,9 @@ move.l (%a1), %a1  /* Set a1 to the location where the result is to be held */
 move.l (%a2), %a2  /* Set a2 to the location where the first array begins */
 move.l (%a3), %a3  /* Set a3 to the locaiton where the second array begins */
 
-move.l (%a2), %d3 /* Move the first element in the first array to 
+move.l (%a2), %d3 /* Move the first element in the first array to
                      data register d3 */
-add.l (%a3), %d3  /* Add the first element in the second array to 
+add.l (%a3), %d3  /* Add the first element in the second array to
                      data register d3 */
 move.l %d3, (%a1) /* Move the sum to the first location of the result array */
 
@@ -75,7 +75,7 @@ clr.l %d2 /* Reset counter to 0           */
 
 loopb:    /* Beginning of loop for part b */
 
-/* At the end of each loop, increments the offset value by 4*n where n is the 
+/* At the end of each loop, increments the offset value by 4*n where n is the
 number of iterations (0 <= n < [number of elements in array]) */
 
 move.l (%d1,%a2), %d3  /* Move the nth element of the first array to d3 */
@@ -106,18 +106,18 @@ clr.l %d2 /* Reset counter to 0           */
 
 loopc:    /* Beginning of loop for part c */
 
-move.l (%a2)+, %d3 /* Move the value at the location pointed at by a2 to d3, 
-                      then increment the address in a2 by the size of 
+move.l (%a2)+, %d3 /* Move the value at the location pointed at by a2 to d3,
+                      then increment the address in a2 by the size of
                       a long word (4) */
-add.l (%a3)+, %d3  /* Add the value at the location pointed at by a3, then  
+add.l (%a3)+, %d3  /* Add the value at the location pointed at by a3, then
                       increment the address in a3 by the size of a long word*/
-move.l %d3, (%a1)+ /* Move the sum to the location pointed at by a1, then  
+move.l %d3, (%a1)+ /* Move the sum to the location pointed at by a1, then
                       increment the address in a1 by the size of a long word*/
 
 add.l #1, %d2        /* Increment counter by 1 */
 cmp.l 0x2300000, %d2 /* Compare the counter to the number of elements */
-bge end              /* If the counter is greater than or equal to the number  
-                        of elements, quit the loop and continue 
+bge end              /* If the counter is greater than or equal to the number
+                        of elements, quit the loop and continue
                         to the next part */
 bra loopc            /* Otherwise repeat the loop */
 
@@ -129,6 +129,6 @@ bra loopc            /* Otherwise repeat the loop */
 end:
 /* DO NOT MODIFY THIS --------------------------------------------*/
 movem.l (%a7),%d2-%d7/%a2-%a5 /*Restore data and address registers */
-lea      40(%a7),%a7 
+lea      40(%a7),%a7
 rts
 /*----------------------------------------------------------------*/
--- Lab 2/code/Lab2b.s
+++ Lab 2/code/Lab2b.s
@@ -14,14 +14,14 @@ movem.l %d2-%d7/%a2-%a5,(%a7)
 /* Names of Students: Arun Woosaree and Navras Kamal             **/
 /* Date: February 12, 2018                                       **/
 /* General Description:                                          **/
-/*                                                               **/
+/* Calculates area under curve using trapezoidal approximation   **/
 /******************************************************************/
 
 /*Write your program here******************************************/
 
 
 move.l 0x2300000, %d0 /* Stores total number of points to d0 */
-sub.l #1, %d0         /* Subtracts one from this value, as one point will be 
+sub.l #1, %d0         /* Subtracts one from this value, as one point will be
                          used in initialization */
 move.l 0x2300004, %a0 /* Stores the location of the first x element in a0 */
 move.l 0x2300008, %a1 /* Stores the location of the first y element in a1 */
@@ -30,7 +30,7 @@ move.l 0x2300010, %a2 /* Stores output location ot a2 */
 clr.l %d1 /* Resets counter variable to 0 */
 clr.l %d7 /* Resets total area to 0       */
 
-/* The area of the trapezoid will be calculated with the formula 
+/* The area of the trapezoid will be calculated with the formula
 ((x1-x0)*(y0+y1))/2 */
 
 move.l (%a0)+, %d2 /* Move the first x0 to d2, then increment a0 by 4 */
@@ -46,12 +46,12 @@ move.l (%a1)+, %d5 /* Store the next y1 into d5, then increment a1 by 4 */
 
 add.l %d5, %d3  /* y0 + y1, overwriting the value in d3 */
 sub.l %d4, %d2  /* x0 - x1, overwriting the value in d2 */
-neg.l %d2       /* Changes the result of the above operation to x1 - x0, 
+neg.l %d2       /* Changes the result of the above operation to x1 - x0,
                    stored in d2 */
-cmp.l #2, %d2   /* The value of (x1-x0) can only be 1 or 2.  Compare 
+cmp.l #2, %d2   /* The value of (x1-x0) can only be 1 or 2.  Compare
                    the value in d2 to 2 */
 bne skp         /* If (x1-x0) is not 2 it is 1, so skip the below operation */
-asl.l #1, %d3   /* If (x1-x0) is 2 multiply the value in d3 by 2 using an 
+asl.l #1, %d3   /* If (x1-x0) is 2 multiply the value in d3 by 2 using an
                    arithmetic shift left by 1 */
 
 skp:            /* Jump here to avoid executing the multiplication, or just
@@ -64,9 +64,9 @@ bra mainloop    /* Restart the main loop */
 
 endloop: /* Go here once the above loop is complete */
 
-/* The code below was a test to make sure the result rounded up.  It is not 
-tested and not refined.  At the request of the TAs it is here in the code but 
-commented out and will not run, as such it is not neccessarily part of our 
+/* The code below was a test to make sure the result rounded up.  It is not
+tested and not refined.  At the request of the TAs it is here in the code but
+commented out and will not run, as such it is not neccessarily part of our
 final answer for this part of the lab */
 
 /*
@@ -74,14 +74,14 @@ move.l %d7, %d6 /* Copy the final answer to d6 */
 and.l #0b1, %d6 /* Reduce it to its least significant bit */
 cmpl #0, %d6    /* Compare that bit to 0 */
 bne skppy       /* If it is zero the number is even, so skip the next line */
-add.l #1, %d7   /* If the value is odd then add 1 to the value in d7, this will 
-                   make it the next highest nearest number, which when divided 
-                   by one will be equivalent to rounding up the division 
+add.l #1, %d7   /* If the value is odd then add 1 to the value in d7, this will
+                   make it the next highest nearest number, which when divided
+                   by one will be equivalent to rounding up the division
                    of the odd value */
 skppy:          /* Code will continue here */
 */
 
-asr.l #1, %d7     /* Divide the overall sum by 2, using an arethmetic shift 
+asr.l #1, %d7     /* Divide the overall sum by 2, using an arethmetic shift
                      right by 1, completing the sum of trapezoidal areas */
 move.l %d7, (%a2) /* Store the final answer at the appropriate location */
 
@@ -93,6 +93,6 @@ move.l %d7, (%a2) /* Store the final answer at the appropriate location */
 
 /* DO NOT MODIFY THIS --------------------------------------------*/
 movem.l (%a7),%d2-%d7/%a2-%a5 /*Restore data and address registers */
-lea      40(%a7),%a7 
+lea      40(%a7),%a7
 rts
 /*----------------------------------------------------------------*/
